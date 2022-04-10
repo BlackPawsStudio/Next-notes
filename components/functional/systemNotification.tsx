41a97e36fca6a22244722db5782552e7a4e05aa1
Artemiy Bal <artemiy.bal@helmes.com>
2022-04-10 22:45:39 +0300

    change text size in edditing note

--- components/functional/systemNotification.tsx
+++ components/functional/systemNotification.tsx
@@ -29,9 +29,7 @@ const SystemNotification = () => {
   }
 
   useEffect(() => {
-    if (!("Notification" in window)) {
-      alert("This browser does not support desktop notification");
-    } else if (Notification.permission === "granted") {
+    if (Notification.permission === "granted") {
       setNotifications();
     } else if (Notification.permission !== "denied") {
       Notification.requestPermission(function (permission) {
